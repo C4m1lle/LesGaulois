c26217e8b1babc3114736326fa0c947bd70f11b4
Camille Magne <mgc4738a@ensfsi.onmicrosoft.com>
2024-09-30 17:44:42 +0200

    tp2fincours

--- LesGaulois/src/personnages/Romain.java
+++ LesGaulois/src/personnages/Romain.java
@@ -3,11 +3,14 @@ package personnages;
 public class Romain {
 	private String nom;
 	private int force;
-
+	private Equipement[] equipements;
+	private int nbEquipement = 0;
+	
 	public Romain(String nom, int force) {
 		assert force >= 0;
 		this.nom = nom;
 		this.force = force;
+		equipements = new Equipement[2];
 	}
 
 	public String getNom() {
@@ -32,6 +35,31 @@ public class Romain {
 			parler("J'abandonne...");
 		}
 		assert force < forcedeb;
+	}
+
+	private void sEquiper(Equipement equipement) {
+		if (nbEquipement == 2){
+			System.out.println("Le soldat " +nom+ " est déjà bien protégé !");
+		}
+		switch(equipement) {
+		case BOUCLIER:
+			if (nbEquipement == 0 || (nbEquipement == 1 && equipements[0]!=Equipement.BOUCLIER)) {
+			System.out.println("Le soldat " + nom + " s'équipe avec un " + equipement.getNom() + ".");
+			}
+			else if(nbEquipement == 1){
+				System.out.println("Le soldat "+nom+ " possède déjà un casque");
+				}
+			break;
+		case CASQUE:
+			if (nbEquipement == 0 || (nbEquipement == 1 && equipements[0]!=Equipement.BOUCLIER)) {
+				System.out.println("Le soldat " + nom + " s'équipe avec un " + equipement.getNom() + ".");
+				}
+				else if(nbEquipement == 1){
+					System.out.println("Le soldat "+nom+ " possède déjà un bouclier");
+					}
+			break;
+		}
+
 	}
 	public static void main(String[] args) {
 		Romain minus = new Romain("Minus",6);
